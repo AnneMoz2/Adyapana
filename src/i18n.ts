8c15002526e6fdb972ad7c76d8de4a61f8910b8d
AnneMoz2 <manasheanne1@gmail.com>
2025-12-03 18:31:38 +1100

    fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,9 +2,9 @@ import {notFound} from 'next/navigation';
 import {getRequestConfig} from 'next-intl/server';
 
 // Import messages statically for Webpack
-import enMessages from './messages/en.json';
-import siMessages from './messages/si.json';
-import taMessages from './messages/ta.json';
+import enMessages from '@/messages/en.json';
+import siMessages from '@/messages/si.json';
+import taMessages from '@/messages/ta.json';
 
 export const locales = ['en', 'si', 'ta'] as const;
 export type Locale = (typeof locales)[number];
